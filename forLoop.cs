854f3596e5f5f96d5c0dfabbc10637465f32fd92
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-03-07 00:42:54 -0600

    Create forLoop.cs
    printing a statements as many we want by using for loop

---
+++ forLoop.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("For loop Example");
+            }
+            Console.ReadLine();
+        }
+    }
+}
